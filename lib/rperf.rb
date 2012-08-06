307466d33c57697b3de91e49c235b6bb55ae7e05
Rex Walters <rw@pobox.com>
2012-08-06 15:30:48 -0700

    Add workload spec

--- lib/rperf.rb
+++ lib/rperf.rb
@@ -3,14 +3,36 @@ require "rperf/version"
 module Rperf
   class Stream
     attr_accessor :blocksize
+    attr_reader :dedupe
 
     def initialize(bs=8192)
       @blocksize ||= bs
       @random = Random.new(22069)
+      @dedupe = 0 # no dedupe by default
     end
 
     def block
-      @random.bytes(blocksize)
+      @lastblock ||= @random.bytes(blocksize)
+
+      if (1 + @random.rand(100)) <= (100 - @dedupe)
+        @last_block = @random.bytes(blocksize)
+      end
+
+      @last_block
+    end
+
+    def dedupe= (percentage)
+      raise unless (0..100).include?(percentage)
+      @dedupe = percentage
+    end
+  end
+
+  class Workload
+    def initialize(type, path, *opts)
+
+      raise ArgumentError, "Invalid workload type" unless
+               [:seq_read, :seq_write, :rand_read, :rand_write, :rand_both].include?(type) 
+
     end
   end
 end
--- spec/tc_stream_spec.rb
+++ spec/tc_stream_spec.rb
@@ -1,10 +1,11 @@
 require 'rperf'
 
 describe Rperf::Stream do
-  let(:stream) { Rperf::Stream.new }
+  let(:stream) { Rperf::Stream.new(16) }
 
   it "should use 8 KiB as the default block size" do
-    stream.blocksize.should == 8192
+    str2 = Rperf::Stream.new
+    str2.blocksize.should == 8192
   end
 
   describe "#block" do
@@ -15,6 +16,36 @@ describe Rperf::Stream do
     it "should return different results on each call" do
       block1 = stream.block
       stream.block.should_not == block1
+    end
+  end
+
+  describe "#dedupe" do
+    it "should accept a dedupe percentage of 0" do
+      expect { stream.dedupe = 0 }.not_to raise_error
+    end
+
+    it "should accept a dedupe percentage of 100" do
+      expect { stream.dedupe = 100 }.not_to raise_error
+    end
+
+    it "should not accept a dedupe percentage > 100" do
+      expect { stream.dedupe = 101 }.to raise_error
+    end
+
+    it "should not accept a dedupe percentage < 0" do
+      expect { stream.dedupe = -1 }.to raise_error
+    end
+
+    it "block should return the same block approximately dedupe percent of the time" do
+      stream.dedupe = 75
+      dupes = prevblock = 0
+      100.times do
+	newblock = stream.block
+	dupes += 1 if newblock == prevblock
+	prevblock = newblock
+      end
+      dupes.should be >= (stream.dedupe * 0.9).to_i
+      dupes.should be <= (stream.dedupe * 1.1).to_i
     end
   end
 end
