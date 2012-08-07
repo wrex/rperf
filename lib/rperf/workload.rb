dab394c4633a26b8ae0a8a772d936b2a3dbaccbc
Rex Walters <rw@pobox.com>
2012-08-06 21:57:24 -0700

    Add Rspec::Workload#next_block

--- lib/rperf/workload.rb
+++ lib/rperf/workload.rb
@@ -1,10 +1,25 @@
 module Rperf
   class Workload
-    def initialize(type, path, *opts)
+    def initialize(threads, blocksize, device_size, type, *opts)
 
       raise ArgumentError, "Invalid workload type" unless
                [:seq_read, :seq_write, :rand_read, :rand_write, :rand_both].include?(type) 
 
+      @stream = Rperf::Stream.new(blocksize)
+      @returned_bytes = 0
+      @device_size = device_size
+      
     end
+
+    def next_block
+      block = @stream.block
+      if block.length + @returned_bytes <= @device_size
+        @returned_bytes += block.length
+        return block
+      else
+        return nil
+      end
+    end
+
   end
 end
--- spec/tc_workload_spec.rb
+++ spec/tc_workload_spec.rb
@@ -1,29 +1,29 @@
 require 'rperf'
 
 describe Rperf::Workload do
-  it "should require a workload type and device size" do
+  it "should require num_threads, blocksize, device_size, and type" do
     expect { Rperf::Workload.new }.to raise_error ArgumentError
-    expect { Rperf::Workload.new(:seq_read) }.to raise_error ArgumentError
+    expect { Rperf::Workload.new(1) }.to raise_error ArgumentError
+    expect { Rperf::Workload.new(1, 1024) }.to raise_error ArgumentError
+    expect { Rperf::Workload.new(1, 1024, 4096) }.to raise_error ArgumentError
   end
 
-  it "should accept a valid workload type" do
-    expect { Rperf::Workload.new(:seq_read, "/tmp/foo") }.not_to raise_error
+  it "should accept 1-1024-W to a 4096 byte device" do
+    expect { Rperf::Workload.new(1, 1024, 4096, :seq_write) }.not_to raise_error
   end
 
   it "should only accept valid IO types" do
-    expect { Rperf::Workload.new(:foobar, "/tmp/foo") }.to raise_error ArgumentError
+    expect { Rperf::Workload.new(1, 1024, 4096, :foobar) }.to raise_error ArgumentError
   end
 
   describe "#next" do
     it "should return device size bytes for sequential writes" do
-      wl = Rperf::Workload.new(:seq_write, "0 KiB", 
-                               :threads => 1,
-                               :blocksize => "1 KiB"
-                               :loop => 0)
+      wl = Rperf::Workload.new(1, 1024, 4096, :seq_write, :loop => false)
       bytes = 0
-      while block = wl.next do
+      while block = wl.next_block do
         bytes += block.length
       end
+      bytes.should == 4096
     end
   end
 
