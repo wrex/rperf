e99c6058db67af3fc47febe3b3c276637a59aff6
Rex Walters <rw@pobox.com>
2012-08-16 12:25:05 -0700

    Added #step! to worker (all specs pass)

--- lib/rperf/worker.rb
+++ lib/rperf/worker.rb
@@ -3,18 +3,43 @@ module Rperf
     attr_reader :blocksize
     attr_reader :device
     attr_reader :generator
+    attr_reader :type
+    attr_reader :order
+    attr_reader :bytes_written
 
-    def initialize(device, blocksize=8192)
+    attr_accessor :one_pass
+
+    def initialize(device, blocksize=8192, type = :write,
+                  order = :sequential, one_pass = true)
       @blocksize = blocksize
       @device = device
       @generator = BlockGenerator.new(blocksize)
+      @type = type
+      @order = order
+      @one_pass = one_pass
+      @bytes_written = 0
+    end
+
+    def loop? 
+      not @one_pass
     end
 
     def run!
-      written = 0
-      while written < device.size do 
+      while @bytes_written < device.size do 
+        step!
+      end
+    end
+
+    def step!
+      if @bytes_written + blocksize <= device.size
         device.file.syswrite(generator.block)
-        written += blocksize
+        @bytes_written += blocksize
+      elsif @one_pass
+        raise EOFError
+      else
+        device.file.seek(0)
+        device.file.syswrite(generator.block)
+        @bytes_written += blocksize
       end
     end
   end
--- spec/tc_worker_spec.rb
+++ spec/tc_worker_spec.rb
@@ -37,20 +37,60 @@ def verify_unique_contents(name, blocksize)
 end
 
 describe Rperf::Worker do
+
   it "should require a Device" do
     expect { Rperf::Worker.new() }.to raise_error ArgumentError
   end
 
-  it "should default to writing sequentially, 8KiB blocksize, random data, no loop " do
-    dev = Rperf::Device.new("tmp/datafile", 32768)
-    worker = Rperf::Worker.new(dev)
-    worker.blocksize.should == 8192
-    worker.run!
-    verify_unique_contents("tmp/datafile", 8192).should == 32768
+  let(:device) { Rperf::Device.new("tmp/datafile", 32768) }
+
+  let(:worker) { Rperf::Worker.new(device) }
+
+  it "should default to 8KiB blocksize" do
+    worker.generator.blocksize.should == 8192
   end
 
+  it "should default to non-looping" do
+    worker.loop?.should be false
+  end
+
+  it "should default to write" do
+    worker.type.should == :write
+  end
+
+  it "should default to sequential ordering" do
+    worker.order.should == :sequential
+  end
+
+  it "should write one block when step is called" do
+    worker.bytes_written.should == 0
+    worker.step!
+    worker.bytes_written.should == 8192
+    worker.device.file.size.should == 8192
+  end
+
+  it "should by fill a file with random data when run is called" do
+    worker.device.file.size.should == 0
+    worker.run!
+    verify_unique_contents("tmp/datafile", 8192).should == 32768
+    worker.device.file.size.should == 32768
+  end
+
+  it "should only let step be called four times on 32K file"  do 
+    4.times { worker.step! }
+    expect { worker.step! }.to raise_error EOFError
+  end
+
+  it "should allow writing more than file length if looping" do 
+    worker.one_pass = false
+    4.times { worker.step! }
+    expect { worker.step! }.not_to raise_error
+    worker.bytes_written.should == 32768 + 8192
+  end
+    
   it "should write blocks in random order when type is write and selector is random"
   it "should loop sequentially when told to"
   it "should only do io to the specified range"
+
 end
 
